1693c08894324fe324249bda636835fb3d5c39ab
Gerardo Garcia <g.garciac@outlook.com>
2022-06-02 16:19:14 -0500

    Solucion ejercicio 002

---
+++ ejercicios/002/g-garciac/Program.cs
@@ -0,0 +1,4 @@
+﻿int Fibo(int n) => n < 2 ? n : Fibo(n - 1) + Fibo(n - 2);
+int SumaParesFibo(int n) =>
+    Enumerable.Range(1, n).Select(Fibo).Where(x => x % 2 is 0).Sum();
+Console.WriteLine(SumaParesFibo(10));
