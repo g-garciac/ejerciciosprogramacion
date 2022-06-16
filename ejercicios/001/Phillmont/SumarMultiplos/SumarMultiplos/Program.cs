fe16985a5e5285f0a09291a42c089dab9bccbcb8
Felipe Montealegre <felipemontealegre@hotmail.com>
2022-06-15 19:26:15 -0500

    Ejercicio 001 y 002

---
+++ ejercicios/001/Phillmont/SumarMultiplos/SumarMultiplos/Program.cs
@@ -0,0 +1,14 @@
+﻿int Sumar(int Limite)
+{
+    int Suma = 0;
+    for (int i = 1; i <= Limite; i++)
+    {
+        if (i % 3 == 0)
+            Suma += i;
+        else if (i % 5 == 0)
+            Suma += i;
+    }
+    return Suma;
+}
+
+Console.WriteLine(Sumar(10));
---
+++ ejercicios/002/Phillmont/EvenFibonnaciNumbers/EvenFibonnaciNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int SumarSerie(int n)
+{
+    int pv = 1;
+    int sv = 1;
+    int suma = 0;
+    for (int a = 2; a <= n; a++)
+    {
+        int x = pv + sv;
+        if (x % 2 == 0)
+            suma += x;
+        pv = sv;
+        sv = x;
+    }
+    return suma;
+}
+
+Console.WriteLine(SumarSerie(20));
