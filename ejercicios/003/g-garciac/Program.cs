99ae1007733c0b0f27eb1a61d3b8624f99af2c86
Gerardo Garcia <g.garciac@outlook.com>
2022-06-22 14:35:34 -0500

    Ejercicioo 003

---
+++ ejercicios/003/g-garciac/Program.cs
@@ -0,0 +1,17 @@
+﻿bool EsPrimo(int n) => n >= 2 && !Enumerable.Range(2, n - 2).Any(i => n % i is 0);
+string Rotar(string s) => $"{s[1..]}{s[..1]}";
+IEnumerable<int> Rotaciones(int n)
+{
+    int NumeroDeDigitos(int n) => n.ToString().Length;
+    yield return n;
+    var rotado = n.ToString();
+    for (var i = 1; i < NumeroDeDigitos(n); i++)
+    {
+        rotado = Rotar(rotado);
+        yield return int.Parse(rotado);
+    }
+}
+bool EsPrimoCircular(int n) => Rotaciones(n).AsParallel().All(EsPrimo);
+IEnumerable<int> ListaPrimos(int n) => Enumerable.Range(0, n - 1).AsParallel().Where(i => EsPrimo(i));
+IEnumerable<int> ListaPrimosCirculares(int n) => ListaPrimos(n).AsParallel().Where(EsPrimoCircular);
+ListaPrimosCirculares(200_000).AsParallel().OrderBy(n => n).ToList().ForEach(Console.WriteLine);
